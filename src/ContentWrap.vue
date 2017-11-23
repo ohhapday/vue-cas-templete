74baaa67547cf87e55cab535a1db55d9b30c46a6
서정석 <ohhapday@naver.com>
2017-11-23 13:05:22 +0900

    11/23 admin-lte  - 템플릿 작업후 git 업로드

--- src/ContentWrap.vue
+++ src/ContentWrap.vue
@@ -56,5 +56,10 @@
 </script>
 
 <style>
-
+  .page-enter-active, .page-leave-active {
+    transition: opacity 0.5s, transform 0.5s;
+  }
+  .page-enter, .page-leave-to {
+    opacity: 0;
+  }
 </style>
